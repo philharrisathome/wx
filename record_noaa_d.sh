567b9f0ee14ed65e47b64cb291f23e22e2d4df1e
phil <phil@philandmel.com>
2016-07-31 17:22:06 +0100

    Working recording using luaradio

--- record_noaa_d.sh
+++ record_noaa_d.sh
@@ -7,7 +7,7 @@ CMD_FIFO=/tmp/record_noaa_cmd
 # Cleanup on exit
 function on_exit {
   echo "Cleaning up..."
-  pkill --signal SIGINT rtl_fm
+  pkill --signal SIGINT luajit
   rm -f $CMD_FIFO
   exit
 }
@@ -19,18 +19,6 @@ trap on_exit SIGHUP SIGINT SIGTERM
 rm -f $CMD_FIFO
 mkfifo $CMD_FIFO
 
-# Setup audio environment for sox
-#export AUDIODRIVER=pulseaudio		# Use PulseAudio driver
-#unset AUDIODEV						# Use default PulseAudio device
-export AUDIODRIVER=alsa				# Use ALSA driver
-export AUDIODEV=hw:Loopback,0,0		# Use ALSA loopback (output will be on hw:Loopback,0,1)
-#export AUDIODEV=hw:1,0,0			# Use built-in audio port
-#export AUDIODEV=hw:2,0,0			# Use USB soundcard
-
-# Resampler parameters
-EFFECTS="rate 11025"
-RATE=48k				# Need to be much higher than maximum deviation to avoid aliasing
-
 # Main loop
 echo "Waiting..."
 while true; do
@@ -41,15 +29,16 @@ while true; do
     if [[ "${params[0]}" == 'start' ]]; then
       echo "`date`: Seen start: "$line
       FILENAME="/home/pi/wx/"$(date +"%Y%m%d%H%M%S")".wav"
-      FILENAME_RAW="/home/pi/wx/"$(date +"%Y%m%d%H%M%S")".raw"
       COMMENT=${params[1]}_$(date +"%Y%^b%d-%H%M%S%Z")
       echo "Recording: "$FILENAME
-      rtl_fm -M fm -f "${params[2]}" -s $RATE -g 50 | (sox -V0 -r $RATE -t raw -e s -b 16 -c 1 - -V0 --comment $COMMENT $FILENAME $EFFECTS) &
+      luaradio rtlsdr_noaa_apt.lua "${params[2]/M/e6}" $FILENAME &
 
     # Look for stop command (e.g. stop)
     elif [[ "${params[0]}" == 'stop' ]]; then
       echo "`date`: Seen stop: "$line
-      pkill --signal SIGINT rtl_fm
+      pkill --signal SIGINT luajit
+      sleep 5s
+      ./process_recording.sh $FILENAME &
 
     # Look for quit command (e.g. quit)
     elif [[ "${params[0]}" == 'quit' ]]; then
---
+++ rtlsdr_noaa_apt.lua
@@ -0,0 +1,32 @@
+local radio = require('radio')
+
+if #arg < 2 then
+    io.stderr:write("Usage: " .. arg[0] .. " <FM radio frequency> <Output filename>\n")
+    os.exit(1)
+end
+
+local frequency = tonumber(arg[1])
+local output_file = tostring(arg[2])
+local tune_offset = -250e3
+local if_bandwidth = 40e3;
+local audio_pipe = '/tmp/noaa_audio_pipe'
+
+io.stderr:write("Recording from " .. frequency / 1e6 .. " MHz to " .. output_file .. "\n")
+
+-- Blocks
+local source = radio.RtlSdrSource(frequency + tune_offset, 1102500, {rf_gain = 50})
+local raw_sink = radio.RawFileSink('noaa.raw')
+local tuner = radio.TunerBlock(tune_offset, if_bandwidth, 10)
+local fm_demod = radio.FrequencyDiscriminatorBlock(1.00) -- Usually 1.25
+local af_filter = radio.LowpassFilterBlock(128, 5.5e3)
+local af_downsampler = radio.DownsamplerBlock(10)		-- Need 11025 as output rate
+--local audio_sink = radio.PulseAudioSink(1)
+local wav_sink = radio.WAVFileSink(output_file,1,16)
+local pipe_sink = radio.RealFileSink(audio_pipe,'s16le')
+
+-- Connections
+local top = radio.CompositeBlock()
+top:connect(source, tuner, fm_demod, af_filter, af_downsampler, wav_sink)
+top:connect(af_downsampler, pipe_sink)
+
+top:run()
